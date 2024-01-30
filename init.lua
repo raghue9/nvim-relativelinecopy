3442c5bcc0200a73354944cab29c9cf1a94d4621
Dell 7000 <erapanee@uni-muenster.de>
2024-01-30 17:36:56 +0100

    various file formating

--- init.lua
+++
@@ -1,5 +0,0 @@
--- nvim-relativelinecopy/init.lua
-local relcopy = require('relativelinecopy')
-
-vim.api.nvim_set_keymap('n', '<leader>cp',
-  [[<Cmd>lua require('relativelinecopy').copyPasteRelativeLine(vim.v.count1)<CR>]], { noremap = true, silent = true })
